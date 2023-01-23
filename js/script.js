1de1d4a7415b4b12eb2e0289b6a1f2d17fbca393
Mariano <macacozzz@hotmail.com>
2023-01-23 14:34:26 -0300

    menu button functionality in js for small screens

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 
+
+/* Set current year */
 const yearEl = document.querySelector('.year');
 yearEl.textContent =  new Date().getFullYear();
+
+
+// Make mobile navigation work
+const btnNavEl = document.querySelector('.btn-mobile-nav');
+const headerEl = document.querySelector('.header');
+btnNavEl.addEventListener('click', () => {
+  headerEl.classList.toggle('nav-open');
+});
+
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
